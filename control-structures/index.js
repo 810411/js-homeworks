d1d8bc3d8fbd7d04487eb8e2a5778e8b98339e16
Grigorii Balabanov <810411@mail.ru>
2018-10-04 15:51:40 +0500

    control-structures done

---
+++ control-structures/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// task_1
+
+function order(count_in_stock, units_for_order) {
+  if (units_for_order > count_in_stock) {
+    console.log('На складе нет такого количества товаров.');
+  } else if (units_for_order === count_in_stock) {
+    console.log('Вы забираете весь товар c нашего склада!');
+  } else {
+    console.log('Заказ оформлен.');
+  }
+}
+
+order(100, 1);
+order(100, 100);
+order(1, 100);
+
+// task_2
+
+var destination = 'Звезда смерти';
+
+switch (destination) {
+  case 'Луна':
+    console.log('Стоимость доставки для области Луна: 150 Q');
+    break;
+  case 'Крабовидная туманность':
+    console.log('Стоимость доставки для области Крабовидная туманность: 250 Q');
+    break;
+  case 'Галактика Туманность Андромеды':
+    console.log('Стоимость доставки для области Галактика Туманность Андромеды: 550 Q');
+    break;
+  case 'Туманность Ориона':
+    console.log('Стоимость доставки для области Туманность Ориона: 600 Q');
+    break;
+  case 'Звезда смерти':
+    console.log('Стоимость доставки для области Звезда смерти: договорная цена');
+    break;
+  default:
+    console.log('В ваш квадрант доставка не осуществляется');
+}
+
+// task_3
+
+var price = 'Полтинник';
+
+try {
+  if (typeof(price) !== 'number') {
+    throw price;
+  }
+  console.log('Цена товара введена корректно');
+}catch (err) {
+    console.log(`Вы допустили ошибку: ${price} не является числом`);
+}
+
+// task_4
+
+var planet_of_residence;
+var age;
+
+if (planet_of_residence === 'Земля' && age < 18) {
+  console.log('Вы не достигли совершеннолетия.')
+}else if (planet_of_residence === 'Земля' && age >= 18) {
+  console.log('Приятных покупок.')
+}else if (planet_of_residence === 'Юпитер' && age < 120) {
+  console.log('Сожалеем. Вернитесь на 120-й день рождения!')
+}else if (planet_of_residence === 'Юпитер' && age >= 120) {
+  console.log('Чистого неба и удачных покупок!')
+}else {
+  console.log('Спасибо, что пользуетесь услугами нашего магазина!')
+}
